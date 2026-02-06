579bdb9a0c85a04fb023d514b392f214252e9ccb
Jeyaneedhan21 <mjeyaneedhan21@gmail.com>
2026-02-06 20:33:32 +0530

    Update Session_Helper.php

--- app/helpers/Session_Helper.php
+++ app/helpers/Session_Helper.php
@@ -4,6 +4,7 @@ session_start();
 
 // Flash message helper - this lets us show one-time messages to users
 // For example, "Successfully registered!" after signing up
+
 // EXAMPLE - flash('register_success', 'You are now registered', 'alert alert-success');
 // DISPLAY IN VIEW - <?php echo flash('register_success');
 function flash($name = '', $message = '', $class = 'msg-flash')
@@ -11,6 +12,7 @@ function flash($name = '', $message = '', $class = 'msg-flash')
     if (!empty($name)) {
         // If we're setting a new flash message
         if (!empty($message) && (!isset($_SESSION[$name]) || empty($_SESSION[$name]))) {
+
             // First, clear out any old messages with the same name, just to be safe
             if (isset($_SESSION[$name]) && !empty($_SESSION[$name])) {
                 unset($_SESSION[$name]);
@@ -20,22 +22,25 @@ function flash($name = '', $message = '', $class = 'msg-flash')
                 unset($_SESSION[$name . '_class']);
             }
 
+            // $name + $message provided
+            // SET MODE: If we provided a $message, we want to save it in the session so it can be displayed on the next page load.
             // Now save the new message and its CSS class
             $_SESSION[$name] = $message;
             $_SESSION[$name . '_class'] = $class;
         } else {
+
+            // only $name provided
             // DISPLAY MODE: If we only provided a $name, we want to show the message.
             if (isset($_SESSION[$name]) && !empty($_SESSION[$name])) {
                 $class = isset($_SESSION[$name . '_class']) && !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
-            echo '<div class="' . $class . '" id="' . $class . '">' . $_SESSION[$name] . '</div>';
-            
-            // IMPORTANT: Unset the values so the message disappears when they refresh.
-            unset($_SESSION[$name]);
+                echo '<div class="' . $class . '" id="' . $class . '">' . $_SESSION[$name] . '</div>';
+
+                // IMPORTANT: Unset the values so the message disappears when they refresh.
+                unset($_SESSION[$name]);
                 if (isset($_SESSION[$name . '_class'])) {
-            unset($_SESSION[$name . '_class']);
+                    unset($_SESSION[$name . '_class']);
                 }
             }
         }
     }
 }
-
